e549c1c24b8e0a1040c0812ffd1935e5ee6d3ad7
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2024-05-15 10:57:26 -0600

    feat: :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { CharacterCarousel } from "./components/characters/carousel";
+import { EpisodeCarousel } from "./components/episodes/carousel";
+import { LocationCarusel } from "./components/locations/carousel";
+import { NavBar } from "./components/utils/navbar";
+import { useAllTypes } from "./hooks/useAllTypes"
 
 function App() {
 
+  const { loading, result: catalog } = useAllTypes();
+
   return (
-    <h1>Hello World</h1>
+    <div data-bs-theme="dark">
+      <div className="bg-body">
+        <NavBar />
+        <main>
+          <CharacterCarousel characters={catalog?.characters.results} />
+          <div className="border-top border-primary"></div>
+          <LocationCarusel locations={catalog?.locations.results} />
+          <div className="border-top border-primary"></div>
+          <EpisodeCarousel episodes={catalog?.episodes.results} />
+        </main>
+      </div>
+    </div>
   )
 }
 
